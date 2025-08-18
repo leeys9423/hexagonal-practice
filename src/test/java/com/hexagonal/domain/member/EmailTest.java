ad7787afd763456b27354fbebe04c3143952de47
leeys9423 <leeys9423@gmail.com>
2025-08-18 21:52:12 +0900

    이메일 형식에 대한 테스트 추가

--- src/test/java/com/hexagonal/domain/member/EmailTest.java
+++ src/test/java/com/hexagonal/domain/member/EmailTest.java
@@ -1,8 +1,10 @@
 package com.hexagonal.domain.member;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class EmailTest {
 
@@ -12,5 +14,29 @@ class EmailTest {
         var email2 = new Email("test@test.com");
 
         assertThat(email1).isEqualTo(email2);
+    }
+
+    @DisplayName("이메일 형식에 맞게 이메일을 객체를 생성해야 한다.")
+    @Test
+    void email_ShouldBeSetCorrectPattern() {
+        //given
+        String validEmail = "test@example.com";
+
+        //when
+        Email email = new Email(validEmail);
+
+        //then
+        assertThat(email.address()).isEqualTo(validEmail);
+    }
+
+    @DisplayName("잘못된 이메일 형식으로는 Email 객체를 생성할 수 없다.")
+    @Test
+    void email_ShouldThrowException_WhenInvalidFormat() {
+        //given
+        String invalidEmail = "invalid-email";
+
+        //when & then
+        assertThatThrownBy(() -> new Email(invalidEmail))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
